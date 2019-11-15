d6f2e822845167e6e5cc7d50fededa93ff3fbb7d
Sriram <pingsrirams@gmail.com>
2019-11-15 13:02:32 -0800

    Updates

---
+++ precache-manifest.a15102e034ff3027f61851d5c86d4592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a6bbe13b8ce13bd51543cfd1bad3c0",
+    "url": "/react-todo-dnd/index.html"
+  },
+  {
+    "revision": "ae58bd143cf748b2c768",
+    "url": "/react-todo-dnd/static/css/2.b0f60dd0.chunk.css"
+  },
+  {
+    "revision": "f299f65674ca5a172a1f",
+    "url": "/react-todo-dnd/static/css/main.36997837.chunk.css"
+  },
+  {
+    "revision": "ae58bd143cf748b2c768",
+    "url": "/react-todo-dnd/static/js/2.43678b22.chunk.js"
+  },
+  {
+    "revision": "f299f65674ca5a172a1f",
+    "url": "/react-todo-dnd/static/js/main.3f72ed94.chunk.js"
+  },
+  {
+    "revision": "0e4cb6e3e100d575a645",
+    "url": "/react-todo-dnd/static/js/runtime-main.00836b51.js"
+  }
+]);
